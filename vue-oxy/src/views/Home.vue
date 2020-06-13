daf7ae57c77b88adc89f2358248f674ed82e36ac
nonobstant <56279981+nonobstant@users.noreply.github.com>
2020-06-13 03:09:26 +0200

    --update

--- vue-oxy/src/views/Home.vue
+++ vue-oxy/src/views/Home.vue
@@ -3,11 +3,21 @@
     <div class="p-5">
       <img :src="require('../assets/'+game.img)" class="mx-auto w-8/12" alt="">
     </div>
-    <div @click="clicker" class="w-full h-full py-6 px-16 cursor-pointer">
-      <p class="text-center" v-html="game.txt"></p><p @click="count = 100" v-if="count === 100">NO</p>
+    <div class="text-center py-3" style="border:solid 1px white">
+      <div @click="clicker" class="w-full h-full px-16 cursor-pointer pb-2">
+        <vue-typer class="" :text="game.txt" :repeat='0' :type-delay='100'></vue-typer>
+      </div>
+      <p class="inline-block border-black border-l-2 px-3 mx-2 hover:border-white cursor-pointer" @click="goto(23)" v-if="count === 22">OUI</p> <br>
+      <p class="inline-block border-black border-l-2 px-3 mx-2 hover:border-white cursor-pointer" @click="goto(43)" v-if="count === 22">NON</p>
+      <p class="inline-block border-black border-l-2 px-3 mx-2 hover:border-white cursor-pointer" @click="goto(32)" v-if="count === 31">OUI</p>
+      <p class="inline-block border-black border-l-2 px-3 mx-2 hover:border-white cursor-pointer" @click="goto(54)" v-if="count === 53">OUI</p>
+      <p class="inline-block border-black border-l-2 px-3 mx-2 hover:border-white cursor-pointer" @click="goto(61)" v-if="count === 60">OUI</p>
     </div>
-      <p @click="backclicker">back</p>
-      <vue-typer class="" :text="game.txt" :repeat='0'></vue-typer>
+
+      <p class="mt-5 pt-10 pb-4">OPTION :</p>
+      <p class="inline-block cursor-pointer" @click="backclicker">back</p>
+      <input class="text-white bg-black float-right" v-model="count" type="text" name="" value="" style="width:5ch">
+
   </div>
 </template>
 
@@ -30,6 +40,9 @@ data() {
       txt : 'START',
       music : ''
     },
+    hana : false,
+    mask : false,
+    key : false,
     count : 1,
   }
 },
@@ -42,10 +55,23 @@ methods: {
     this.count --
     this.refresh()
   },
+  goto(x) {
+    if (this.count === 31) {
+      this.hana = true
+    } else if (this.count === 0) {
+      this.mask = true
+    } else if (this.count === 0) {
+      this.key = true
+    } else {
+      console.log("-- --- .-. - (22)")
+    }
+    this.count = x
+    this.refresh()
+  },
   refresh() {
     if (this.count === 1) {
       this.game.img = 'archives/bougie.png'
-      this.game.txt = '<span class="text-red-500">Allume \n la bougie.</span>'
+      this.game.txt = 'Allume la bougie.'
       this.game.music = 'feu'
     } else if (this.count === 2) {
       this.game.img = 'archives/bougie-a.png'
@@ -59,7 +85,7 @@ methods: {
       this.game.txt = '. . .'
     } else if (this.count === 5) {
       this.game.img = 'archives/6.gif'
-      this.game.txt = "La chance est dans l’oeil de celui qui le regarde. <br>Elle n’est que l’admiration du hasard."
+      this.game.txt = "La chance est dans l’oeil de celui qui regarde. \nElle n’est que l’admiration du hasard."
     } else if (this.count === 6) {
       this.game.img = 'archives/77.gif'
       this.game.txt = 'L’observation de la nature,'
@@ -110,7 +136,7 @@ methods: {
 
     } else if (this.count === 18) {
         this.game.img = 'archives/AH.png'
-        this.game.txt = '<span class="text-red-500 font-bold">Ah.</span>'
+        this.game.txt = 'Ah.'
 
     } else if (this.count === 19) {
         this.game.img = 'archives/you-stepped.png'
@@ -121,170 +147,175 @@ methods: {
         this.game.txt = 'Devant vous, se tient une porte.'
 
 
-    }else if (this.count === 21) {
-        this.game.img = 'archives/DOORGIF.gif'
-        this.game.txt = 'Ouvrir la porte (oui)'
+    } else if (this.count === 21) {
+        this.game.img = 'archives/DOORGIF2.gif'
+        this.game.txt = 'Devant vous, se tient une porte.'
 
 
     } else if (this.count === 22) {
+        this.game.img = 'archives/DOORGIF.gif'
+        this.game.txt = 'Ouvrir la porte?'
+
+
+    } else if (this.count === 23) {
         this.game.img = 'nature/1-chemin.png'
         this.game.txt = 'Vous êtes sorti dehors.'
 
-    } else if (this.count === 23) {
+    } else if (this.count === 24) {
         this.game.img = 'nature/1.gif'
         this.game.txt = 'La lumière est si dense dehors.'
 
-    }else if (this.count === 24) {
+    }else if (this.count === 25) {
         this.game.img = 'nature/herb-and-trees.gif'
         this.game.txt = 'L’atmosphère est si douce,'
 
-    }else if (this.count === 25) {
+    }else if (this.count === 26) {
         this.game.img = 'nature/rainbow.gif'
         this.game.txt = 'Le temps semble s’être arrêté.'
 
-    }else if (this.count === 26) {
+    }else if (this.count === 27) {
         this.game.img = 'nature/cheminn.png'
         this.game.txt = 'Vous apercevez un petit chemin.'
 
-    }else if (this.count === 27) {
+    }else if (this.count === 28) {
         this.game.img = 'nature/nature-vive-les-cailloux.gif'
         this.game.txt = 'L’endroit est paisible.'
 
-    }else if (this.count === 28) {
+    }else if (this.count === 29) {
         this.game.img = 'nature/nature-to-city.gif'
         this.game.txt = '. . .'
 
-    }else if (this.count === 29) {
+    }else if (this.count === 30) {
         this.game.img = 'nature/the-tree.png'
         this.game.txt = 'Cet arbre est particulier.'
 
-    }else if (this.count === 30) {
+    }else if (this.count === 31) {
         this.game.img = 'nature/the-tree-2.png'
         this.game.txt = '*cueillir ?*'
 
-    }else if (this.count === 31) {
-        this.game.img = 'nature/the-tree-3.png'
-        this.game.txt = 'Garde le précieusement.<br> Ce fruit n’est pas comme les autres.'
-
-
     }else if (this.count === 32) {
+        this.game.img = 'nature/the-tree-3.png'
+        this.game.txt = 'Garde le précieusement. \nCe fruit n’est pas comme les autres.'
+
+
+    }else if (this.count === 33) {
         this.game.img = 'nature/rain.png'
         this.game.txt = '. . . Il semblerait que la nuit commence à tomber.'
 
-    }else if (this.count === 33) {
+    }else if (this.count === 34) {
         this.game.img = 'nature/paintt.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 34) {
+    }else if (this.count === 35) {
         this.game.img = 'nature/amen.gif'
         this.game.txt = 'Il y aurait peut-être moyen de s’abriter là-dedans...?'
 
 
-    }else if (this.count === 35) {
+    }else if (this.count === 36) {
         this.game.img = 'nature/rouille.png'
         this.game.txt = 'Où suis-je ?'
 
-    }else if (this.count === 36) {
+    }else if (this.count === 37) {
         this.game.img = 'nature/trous2.png'
         this.game.txt = 'Ce passage mène à un endroit...'
 
-    }else if (this.count === 37) {
+    }else if (this.count === 38) {
         this.game.img = 'nature/trous.gif'
         this.game.txt = 'Mais... ?'
 
-    }else if (this.count === 38) {
+    }else if (this.count === 39) {
         this.game.img = 'nature/vie-et-mort.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 39) {
+    }else if (this.count === 40) {
         this.game.img = 'metropole/cat3.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 40) {
+    }else if (this.count === 41) {
         this.game.img = 'metropole/cat2.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 41) {
+    }else if (this.count === 42) {
         this.game.img = 'metropole/cat4.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 42) {
+    }else if (this.count === 43) {
         this.game.img = 'metropole/metropole.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 43) {
+    }else if (this.count === 44) {
         this.game.img = 'metropole/void1.gif'
         this.game.txt = 'Gris... Le monde est si gris...'
 
-    }else if (this.count === 44) {
+    }else if (this.count === 45) {
         this.game.img = 'metropole/void2.gif'
         this.game.txt = 'Si bruyant et pollué. On en mourrait d’asphyxie.'
 
-    }else if (this.count === 45) {
-        this.game.img = 'metropole/void3.gif'
-        this.game.txt = 'Tant d’humains concentrés dans un espace si réduit. Tous les jours se ressemblent. Toutes les semaines la même routine. On perd notre vie à la gagner. Cette spirale infernale, la pyramide sociale. Ici, c’est de pire en pire. Un monde tombé dans l’indécence, plein de questions sans réponses.'
-
     }else if (this.count === 46) {
+        this.game.img = 'metropole/void3.gif'
+        this.game.txt = 'Tant d’humains concentrés dans un espace si réduit. \nTous les jours se ressemblent. \nToutes les semaines la même routine. \nOn perd notre vie à la gagner. \nCette spirale infernale, la pyramide sociale. \nIci, c’est de pire en pire. \nUn monde tombé dans l’indécence, plein de questions sans réponses.'
+
+    }else if (this.count === 47) {
         this.game.img = 'metropole/void4.gif'
         this.game.txt = 'Comment arrêter le temps ?'
 
-    }else if (this.count === 47) {
+    }else if (this.count === 48) {
         this.game.img = 'metropole/void44.gif'
         this.game.txt = 'Tout cela est imaginaire.'
 
-    }else if (this.count === 48) {
+    }else if (this.count === 49) {
         this.game.img = 'metropole/why.gif'
         this.game.txt = '. . .'
 
-    }else if (this.count === 49) {
+    }else if (this.count === 50) {
         this.game.img = 'metropole/boom.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 50) {
+    }else if (this.count === 51) {
         this.game.img = 'metropole/cat5.png'
         this.game.txt = 'Pourquoi... Acceptons-nous juste les choses ?'
 
-    }else if (this.count === 51) {
+    }else if (this.count === 52) {
         this.game.img = 'metropole/antenna.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 52) {
-        this.game.img = 'metropole/mask.png'
-        this.game.txt = 'Veux-tu ramasser le masque ?(oui/non)'
-
     }else if (this.count === 53) {
+        this.game.img = 'metropole/mask.png'
+        this.game.txt = 'Veux-tu ramasser le masque ?'
+
+    }else if (this.count === 54) {
         this.game.img = 'metropole/mask2.gif'
         this.game.txt = '. . . Merci.'
 
-    }else if (this.count === 54) {
+    }else if (this.count === 55) {
         this.game.img = 'metropole/cat7.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 55) {
+    }else if (this.count === 56) {
         this.game.img = 'metropole/cat8.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 56) {
+    }else if (this.count === 57) {
         this.game.img = 'metropole/cat.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 57) {
+    }else if (this.count === 58) {
         this.game.img = 'metropole/casier.png'
         this.game.txt = '. . .'
 
-    }else if (this.count === 58) {
+    }else if (this.count === 59) {
           this.game.img = 'metropole/look.gif'
           this.game.txt = 'Prends ton temps.'
 
-    }else if (this.count === 59) {
+    }else if (this.count === 60) {
           this.game.img = 'metropole/clé.png'
           this.game.txt = '*prendre la clé*'
 
-    }else if (this.count === 60) {
+    }else if (this.count === 61) {
           this.game.img = 'metropole/clé2.png'
           this.game.txt = '. . .'
 
-    }else if (this.count === 61) {
+    }else if (this.count === 62) {
           this.game.img = 'metropole/the-end-mothefucka.gif'
           this.game.txt = '. . . entre.'
 
