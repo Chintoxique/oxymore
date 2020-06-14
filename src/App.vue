50f4ef8026c3e4f70db5e369183d0363acd11af5
Chintoxique <57821149+Chintoxique@users.noreply.github.com>
2020-06-14 22:32:12 +0200

    dir--fix

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="mx-64 text-white"  id="app">
+    <div id="nav">
+      <router-link to="/">Oxymore</router-link> /
+      <router-link to="/archives">Archives</router-link>
+    </div>
+
+    <router-view/>
+
+
+
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  background-color: black;
+}
+</style>
---
+++ src/views/sampleee.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about ">
+
+  </div>
+</template>
+<script>
+    //import axios from "axios";
+    export default {
+        components: {
+
+        },
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+        }
+    }
+</script>
